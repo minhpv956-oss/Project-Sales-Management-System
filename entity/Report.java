db0b18331fda34d12c18ccaf0523ac517f2bdd18
Duck19k <109516417+QuanTrannj@users.noreply.github.com>
2026-06-07 00:45:11 +0700

    Update Report.java
    done

--- entity/Report.java
+++ entity/Report.java
@@ -2,15 +2,16 @@
 package entity;
 
 import java.util.Date;
+import java.util.ArrayList;
 
-
-public class Report {
+public class Report extends Customer{
     
     private String reportType;
     private Date reportDate;
     
-    public Report(String reportType,Date reportDate){
-       this.reportDate = reportDate;
+    public Report(String reportType,Date reportDate, String id, String name, String address, String phone){
+  //     super(id, name, address, phone);
+        this.reportDate = reportDate;
        this.reportType = reportType;
         
     }
@@ -31,6 +32,62 @@ public class Report {
         this.reportDate = reportDate;
     }
     
-            
-            
+    
+    public void saleReport(ArrayList<SalesTransaction> transactions)
+    {
+        double totalRevenue = 0;
+
+        for(int i = 0; i < transactions.size(); i++)
+        {
+            totalRevenue += transactions.get(i).getTotalAmount();
+        }
+
+        System.out.println("---------SALES REPORT---------");
+        System.out.println("Report Type: " + reportType);
+        System.out.println("Report Date: " + reportDate);
+        System.out.println("Total Transactions: " + transactions.size());
+        System.out.println("Total Revenue: " + totalRevenue);
+    }
+
+    
+    public void bestSellingProducts(ArrayList<SalesTransaction> transactions)
+    {
+        String bestProductId = "";
+        int maxQuantity = 0;
+
+        for(int i = 0; i < transactions.size(); i++)
+        {
+            if(transactions.get(i).getQuantity() > maxQuantity)
+            {
+                maxQuantity = transactions.get(i).getQuantity();
+                bestProductId = transactions.get(i).getProductId();
+            }
+        }
+
+        System.out.println("------------BEST SELLING PRODUCT------------");
+        System.out.println("Product ID: " + bestProductId);
+        System.out.println("Quantity Sold: " + maxQuantity);
+    }
+
+  
+    public void topCustomers(ArrayList<SalesTransaction> transactions)
+    {
+        String bestCustomerId = "";
+        double maxAmount = 0;
+        int a = 0;
+
+        for(int i = 0; i < transactions.size(); i++)
+        {
+            if(transactions.get(i).getTotalAmount() > maxAmount)
+            {
+                maxAmount = transactions.get(i).getTotalAmount();
+                bestCustomerId = transactions.get(i).getCustomerId();
+                a = i;
+            }
+        }
+
+        System.out.println("------------TOP CUSTOMER------------");
+        System.out.println("Customer ID: " + bestCustomerId);
+        System.out.println("Purchase Value: " + maxAmount);
+    }
 }
